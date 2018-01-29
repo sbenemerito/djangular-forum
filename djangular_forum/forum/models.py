e48d0ca6852ee5dc3ebc570fe2a29c9356f8bb3b
sbenemerito <benemeritosam@gmail.com>
2018-01-29 16:05:23 +0800

    added forum models - included in admin

--- djangular_forum/forum/models.py
+++ djangular_forum/forum/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=500)
+    posted = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{0} - {1}'.format(self.title, self.author)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=250)
+    posted = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    parent_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return "{1}'s comment to [{0}]".format(self.parent_post, self.author)
+
