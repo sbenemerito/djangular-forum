eb7105875dafa0c46b7f251be05f5cc20ba197a0
sbenemerito <benemeritosam@gmail.com>
2018-01-29 16:50:45 +0800

    added SlugField to Post model for title

--- djangular_forum/forum/models.py
+++ djangular_forum/forum/models.py
@@ -1,16 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, editable=False)
     content = models.TextField(max_length=500)
     posted = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '{0} - {1}'.format(self.title, self.author)
+
+    def _get_unique_slug(self):
+        slug = slugify(self.title)
+        unique_slug = slug
+        num = 2
+        while Post.objects.filter(slug=unique_slug).exists():
+            unique_slug = '{}-{}'.format(slug, num)
+            num += 1
+        return unique_slug
+ 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self._get_unique_slug()
+        super().save()
 
 
 class Comment(models.Model):
